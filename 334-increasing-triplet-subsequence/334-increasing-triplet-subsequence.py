c67d5b6c258c4c5b03d26ed0d88594086a9a427e
saurabh jain <sjbpr1@gmail.com>
2022-04-18 16:43:49 +0530

    Time: 606 ms (80.67%), Space: 24.6 MB (47.72%) - LeetHub

--- 334-increasing-triplet-subsequence/334-increasing-triplet-subsequence.py
+++ 334-increasing-triplet-subsequence/334-increasing-triplet-subsequence.py
@@ -1,24 +1,36 @@
 class Solution:
     def increasingTriplet(self, nums: List[int]) -> bool:
-        fir=sec=float("-inf")
-        fir=nums[0]
+     
+        fir=sec=float("inf")
         n=len(nums)
-        i=1
-        while i<n and sec==float("-inf"):
+        i=0
+        while i<n:
             if nums[i]<=fir:
                 fir=nums[i]
-            else:
+            elif nums[i]<=sec:
                 sec=nums[i]
-            i+=1
-        if i==n:
-            return False
-        while i<n:
-            if nums[i]>sec:
+            else:
                 return True
-            elif nums[i]>fir :
-                sec=nums[i]
-            else:
-                fir=nums[i]
             i+=1
         return False    
+#         fir=nums[0]
+#         n=len(nums)
+#         i=1
+#         while i<n and sec==float("-inf"):
+#             if nums[i]<=fir:
+#                 fir=nums[i]
+#             else:
+#                 sec=nums[i]
+#             i+=1
+#         if i==n:
+#             return False
+#         while i<n:
+#             if nums[i]>sec:
+#                 return True
+#             elif nums[i]>fir :
+#                 sec=nums[i]
+#             else:
+#                 fir=nums[i]
+#             i+=1
+#         return False    
         
