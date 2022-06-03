54a44008da7822b3dd0981520bae8a1438e89f46
saurabh jain <sjbpr1@gmail.com>
2022-06-03 21:09:39 +0530

    Time: 7056 ms (21.38%), Space: 52.9 MB (62.18%) - LeetHub

---
+++ 204-count-primes/204-count-primes.py
@@ -0,0 +1,14 @@
+class Solution:
+    def countPrimes(self, n: int) -> int:
+        dp=[False]*(n)
+        if n<2:
+            return 0
+        counti=0
+        for i in range(2,n):
+            if dp[i]==False:
+                counti+=1
+                j=i+i
+                while j<n:
+                    dp[j]=True
+                    j+=i
+        return counti            
