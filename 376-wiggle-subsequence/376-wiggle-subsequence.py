a5b453bad434c3c69ebb77aced7d64e64094375a
saurabh jain <sjbpr1@gmail.com>
2022-07-03 13:29:26 +0530

    Time: 61 ms (44.09%), Space: 13.9 MB (77.55%) - LeetHub

--- 376-wiggle-subsequence/376-wiggle-subsequence.py
+++ 376-wiggle-subsequence/376-wiggle-subsequence.py
@@ -1,17 +1,33 @@
 class Solution:
     def wiggleMaxLength(self, nums: List[int]) -> int:
-        n=len(nums)
-        dp=[[1]*2 for i in range(n)]
+        ###dp
+#         n=len(nums)
+#         dp=[[1]*2 for i in range(n)]
         
-        maxi=1
-        for i in range(1,n):
-            po=ne=0
-            for j in range(i-1,-1,-1):
-                if nums[j]>nums[i]:
-                    ne=max(ne,dp[j][1])
-                elif nums[j]<nums[i]:
-                    po=max(po,dp[j][0])
-            dp[i][1]+=po
-            dp[i][0]+=ne
-            maxi=max(maxi,dp[i][1],dp[i][0])
-        return maxi         
+#         maxi=1
+#         for i in range(1,n):
+#             po=ne=0
+#             for j in range(i-1,-1,-1):
+#                 if nums[j]>nums[i]:
+#                     ne=max(ne,dp[j][1])
+#                 elif nums[j]<nums[i]:
+#                     po=max(po,dp[j][0])
+#             dp[i][1]+=po
+#             dp[i][0]+=ne
+#             maxi=max(maxi,dp[i][1],dp[i][0])
+#         return maxi         
+
+          ###o(N) solution
+        n=len(nums)
+        if n<2:
+            return n
+        pred=nums[1]-nums[0]
+        counti=1
+        if pred!=0:
+            counti+=1
+        for i in range(2,n):
+            dif=nums[i]-nums[i-1]
+            if (dif>0 and pred<=0) or (dif<0 and pred>=0):
+                counti+=1
+                pred=dif
+        return counti        
