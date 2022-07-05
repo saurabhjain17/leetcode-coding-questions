d64d7ab2170bf739ec7897e1e074dadc277c436d
saurabh jain <sjbpr1@gmail.com>
2022-07-05 13:49:20 +0530

    Time: 9782 ms (5.02%), Space: 14 MB (50.78%) - LeetHub

---
+++ 79-word-search/79-word-search.py
@@ -0,0 +1,32 @@
+class Solution:
+    def dfs(self,board,word,ro,co,ind,vis,n,r,c):
+       
+        if ind==n-1:
+            if board[r][c]==word[ind]:
+                return True
+            return False
+        if ind==n:
+            return True
+        
+        for tem in [[1,0],[0,1],[-1,0],[0,-1]]:
+            left=r+tem[0]
+            right=c+tem[1]
+            if left>-1 and left<ro and right>-1 and right<co and (left,right) not in vis and board[left][right]==word[ind+1]:
+                vis.add((left,right))
+                if  self.dfs(board,word,ro,co,ind+1,vis,n,left,right)==True:
+                    return True
+                vis.discard((left,right))
+        return False        
+                
+    def exist(self, board: List[List[str]], word: str) -> bool:
+        ro=len(board)
+        co=len(board[0])
+        n=len(word)
+        for i in range(ro):
+            for j in range(co):
+                if board[i][j]==word[0]:
+                    vis=set()
+                    vis.add((i,j))
+                    if self.dfs(board,word,ro,co,0,vis,n,i,j)==True:
+                        return True
+        return False            
