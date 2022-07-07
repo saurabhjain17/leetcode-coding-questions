859c122bfe48df947465b32f3d2fe002920eec26
saurabh jain <sjbpr1@gmail.com>
2022-07-07 10:51:13 +0530

    Time: 3801 ms (54.95%), Space: 19.7 MB (69.44%) - LeetHub

--- 2304-minimum-path-cost-in-a-grid/2304-minimum-path-cost-in-a-grid.py
+++ 2304-minimum-path-cost-in-a-grid/2304-minimum-path-cost-in-a-grid.py
@@ -3,13 +3,14 @@ class Solution:
     def minPathCost(self, grid: List[List[int]], moveCost: List[List[int]]) -> int:
         m=len(grid)
         n=len(grid[0])
-        dp=[[math.inf]*n for i in range(m)]
-        for i in range(n):
-            dp[0][i]=grid[0][i]
+        dp=grid[0].copy()
+        
         for i in range(1,m):
+            tem=[0]*n
             for j in range(n):
                 ans=math.inf
                 for k in range(n):
-                    ans=min(ans,moveCost[grid[i-1][k]][j]+dp[i-1][k])
-                dp[i][j]=ans+grid[i][j]
-        return min(dp[-1])        
+                    ans=min(ans,moveCost[grid[i-1][k]][j]+dp[k])
+                tem[j]=ans+grid[i][j]
+            dp=tem    
+        return min(dp)        
