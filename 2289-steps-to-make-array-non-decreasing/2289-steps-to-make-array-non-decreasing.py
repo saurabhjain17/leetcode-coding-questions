b7de62f4b638fa5eb8044b6c966b14ff911561fe
saurabh jain <sjbpr1@gmail.com>
2022-07-09 09:29:47 +0530

    Time: 894 ms (96.64%), Space: 29.1 MB (41.32%) - LeetHub

---
+++ 2289-steps-to-make-array-non-decreasing/2289-steps-to-make-array-non-decreasing.py
@@ -0,0 +1,10 @@
+class Solution:
+    def totalSteps(self, A: List[int]) -> int:
+        n = len(A)
+        dp = [0] * n
+        stack = []
+        for i in range(n-1, -1, -1):
+            while stack and A[i] > A[stack[-1]]:
+                dp[i] = max(dp[i] + 1, dp[stack.pop()])
+            stack.append(i)
+        return max(dp)
