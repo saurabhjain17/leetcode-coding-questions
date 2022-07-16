6b1a2af9ffbdcfd63699fada2251c90d7f04f839
saurabh jain <sjbpr1@gmail.com>
2022-07-16 11:58:13 +0530

    Time: 1902 ms (29.91%), Space: 28.8 MB (42.99%) - LeetHub

--- 2208-minimum-operations-to-halve-array-sum/2208-minimum-operations-to-halve-array-sum.py
+++ 2208-minimum-operations-to-halve-array-sum/2208-minimum-operations-to-halve-array-sum.py
@@ -1,18 +1,41 @@
 import heapq
 class Solution:
+    # def halveArray(self, nums: List[int]) -> int:
+    #     n=len(nums)
+    #     sumi=sum(nums)
+    #     sumi_want=sumi
+    #     for i in range(n):
+    #         nums[i]=-nums[i]
+    #     heapq.heapify(nums)
+    #     counti=0
+    #     while sumi_want*2>sumi:
+    #         counti+=1
+    #         number=-heapq.heappop(nums)
+    #         sumi_want-=number
+    #         number/=2
+    #         sumi_want+=number
+    #         heapq.heappush(nums,-number)
+    #     return counti    
     def halveArray(self, nums: List[int]) -> int:
-        n=len(nums)
-        sumi=sum(nums)
-        sumi_want=sumi
-        for i in range(n):
-            nums[i]=-nums[i]
-        heapq.heapify(nums)
-        counti=0
-        while sumi_want*2>sumi:
-            counti+=1
-            number=-heapq.heappop(nums)
-            sumi_want-=number
-            number/=2
-            sumi_want+=number
-            heapq.heappush(nums,-number)
-        return counti    
+        # Creating empty heap
+        maxHeap = []
+        heapify(maxHeap) # Creates minHeap 
+        
+        totalSum = 0
+        for i in nums:
+            # Adding items to the heap using heappush
+            # for maxHeap, function by multiplying them with -1
+            heappush(maxHeap, -1*i) 
+            totalSum += i
+        
+        requiredSum = totalSum / 2
+        minOps = 0
+        
+        while totalSum > requiredSum:
+            x = -1*heappop(maxHeap) # Got negative value make it positive
+            x /= 2
+            totalSum -= x
+            heappush(maxHeap, -1*x) 
+            minOps += 1
+        
+        return minOps
