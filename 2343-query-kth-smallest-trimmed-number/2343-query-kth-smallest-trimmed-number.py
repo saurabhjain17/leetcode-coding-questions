dc77797faf344b92d1adfdbfe16f54423ea2d281
saurabh jain <sjbpr1@gmail.com>
2022-07-18 21:12:23 +0530

    Time: 1207 ms (22.22%), Space: 14.1 MB (22.22%) - LeetHub

--- 2343-query-kth-smallest-trimmed-number/2343-query-kth-smallest-trimmed-number.py
+++ 2343-query-kth-smallest-trimmed-number/2343-query-kth-smallest-trimmed-number.py
@@ -2,8 +2,8 @@ import heapq
 class Solution:
     def smallestTrimmedNumbers(self, nums: List[str], queries: List[List[int]]) -> List[int]:
         n=len(nums)
-        for i in range(n):
-            nums[i]=int(nums[i])
+        # for i in range(n):
+        #     nums[i]=int(nums[i])
         m=len(queries)
         ans=[-1]*m
         for i in range(m):
@@ -11,7 +11,7 @@ class Solution:
             heap=[]
             po=10**trim
             for j in range(n):
-                heapq.heappush(heap,(nums[j]%po,j))
+                heapq.heappush(heap,(nums[j][-trim:],j))
             for j in range(k):
                 nod,ind=heapq.heappop(heap)
             ans[i]=ind
