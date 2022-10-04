1cf28db59c921f6cd9b4f4b427c4d435ee831bb3
saurabh jain <sjbpr1@gmail.com>
2022-10-04 23:52:46 +0530

    Time: 108 ms (90.99%), Space: 14.2 MB (42.94%) - LeetHub

--- 2104-sum-of-subarray-ranges/2104-sum-of-subarray-ranges.py
+++ 2104-sum-of-subarray-ranges/2104-sum-of-subarray-ranges.py
@@ -1,11 +1,48 @@
 class Solution:
     def subArrayRanges(self, nums: List[int]) -> int:
-        sumi=0
         n=len(nums)
+        st=[]
+        left=[1]*n
+        st.append(0)
+        for i in range(1,n):
+            while st and nums[st[-1]]>=nums[i]:
+                st.pop(-1)
+            left[i]=i+1
+            if st:
+                left[i]=i-st[-1]
+            st.append(i)
+        right=[1]*n
+        st=[n-1]
+        for i in range(n-2,-1,-1):
+            while st and nums[st[-1]]>nums[i]:
+                st.pop(-1)
+            right[i]=n-i
+            if st:
+                right[i]=st[-1]-i
+            st.append(i)  
+        res=0
         for i in range(n):
-            mini=maxi=nums[i]
-            for j in range(i+1,n):
-                mini=min(mini,nums[j])
-                maxi=max(maxi,nums[j])
-                sumi+=(maxi-mini)
-        return sumi        
+            res-=nums[i]*left[i]*right[i]
+        st=[]
+        left=[1]*n
+        st.append(0)
+        for i in range(1,n):
+            while st and nums[st[-1]]<=nums[i]:
+                st.pop(-1)
+            left[i]=i+1
+            if st:
+                left[i]=i-st[-1]
+            st.append(i)
+        right=[1]*n
+        st=[n-1]
+        for i in range(n-2,-1,-1):
+            while st and nums[st[-1]]<nums[i]:
+                st.pop(-1)
+            right[i]=n-i
+            if st:
+                right[i]=st[-1]-i
+            st.append(i)     
+        # print(left,right)    
+        for i in range(n):
+            res+=nums[i]*left[i]*right[i]
+        return res        
