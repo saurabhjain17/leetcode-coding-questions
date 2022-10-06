f8c4b1de54a37ef7697563a216b0b23a1b4fe80b
saurabh jain <sjbpr1@gmail.com>
2022-10-06 10:48:38 +0530

    Time: 2461 ms (10.27%), Space: 36.5 MB (5.68%) - LeetHub

---
+++ 1438-longest-continuous-subarray-with-absolute-diff-less-than-or-equal-to-limit/1438-longest-continuous-subarray-with-absolute-diff-less-than-or-equal-to-limit.py
@@ -0,0 +1,14 @@
+import heapq
+class Solution:
+     def longestSubarray(self, A, limit):
+        maxq, minq = [], []
+        res = i = 0
+        for j, a in enumerate(A):
+            heapq.heappush(maxq, [-a, j])
+            heapq.heappush(minq, [a, j])
+            while -maxq[0][0] - minq[0][0] > limit:
+                i = min(maxq[0][1], minq[0][1]) + 1
+                while maxq[0][1] < i: heapq.heappop(maxq)
+                while minq[0][1] < i: heapq.heappop(minq)
+            res = max(res, j - i + 1)
+        return res
