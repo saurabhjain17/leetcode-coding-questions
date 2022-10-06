3437ce79d57cf8c08156a0fe4e29b0b4efa5cd78
saurabh jain <sjbpr1@gmail.com>
2022-10-06 10:53:04 +0530

    Time: 285 ms (9.98%), Space: 18.7 MB (71.21%) - LeetHub

--- 347-top-k-frequent-elements/347-top-k-frequent-elements.py
+++ 347-top-k-frequent-elements/347-top-k-frequent-elements.py
@@ -8,21 +8,10 @@ class Solution:
             if i not in dic:
                 dic[i]=0
             dic[i]+=1
-        t=k    
+        ans=[]
         heap=[]
-        heapq.heapify(heap)
         for i in dic:
-            if t>0:
-                heapq.heappush(heap,(dic[i],i))
-                t-=1
-            else:
-                key,value=heapq.heappop(heap)
-                if key>dic[i]:
-                    heapq.heappush(heap,(key,value))
-                else:
-                    heapq.heappush(heap,(dic[i],i))
-        op=[]    
+            heapq.heappush(heap,(-dic[i],i))
         for i in range(k):
-            key,value=heapq.heappop(heap)
-            op.append(value)
-        return op    
+            ans.append(heapq.heappop(heap)[1])
+        return ans    
